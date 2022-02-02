28958c97f15213758a018c0093b6d2ac9ec1ade5
veselints <veselintsvetanov@gmail.com>
2022-02-03 00:51:07 +0200

    chore: final data

--- crown-caps/src/components/AddPlate.vue
+++ crown-caps/src/components/AddPlate.vue
@@ -44,7 +44,7 @@ export default {
   },
 
   mounted() {
-    this.importAll(require.context('../assets/large/', true, /\.jpg$/));
+    //this.importAll(require.context('../assets/large/', true, /\.jpg$/));
   },
 
   methods: {
